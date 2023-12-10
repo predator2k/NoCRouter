cfbba4e81965b9ced70c24600d802bbf6f327503
MH Nie <mhnie2000@outlook.com>
2023-12-10 21:56:07 +0800

    fix dla to router flit

--- src/rtl/bridge/fifo_router_bridge.sv
+++ src/rtl/bridge/fifo_router_bridge.sv
@@ -63,35 +63,44 @@ end
 // 
 // =================================================================================
 
-enum logic  [2:0]  {IDLE,HEAD_STATE,BODY_STATE,TAIL_STATE,FETCH_INST} ss, ss_next;
-logic       [7:0]           cnt, cnt_next;
+enum logic  [2:0]  {IDLE,HEAD_STATE,BODY_STATE,TAIL_STATE,FETCH_HEAD} ss, ss_next;
+logic       [7:0]           cnt, cnt_next, cnt_d;
 flit_t                      router_data_in_next;
 logic                       router_valid_in_next;
 logic                       dla2noc_fifo_ren_next;
 logic       [7:0]           dla2noc_data_len,dla2noc_data_len_next;
+logic                       dla2noc_fifo_rempty_d,dla2noc_fifo_ren_d;
 
 always_ff @(posedge clk_router or posedge rst_router) begin
     if(rst_router) begin
         ss                             <= IDLE;    
-        cnt                            <= 5'b0;
+        cnt                            <= 8'b0;
+        cnt_d                          <= 8'b0;
         router_data_in.flit_label      <= HEADTAIL;
         router_data_in.vc_id           <= 0;
         router_data_in.data            <= {FLIT_DATA_SIZE{1'b0}};
         router_valid_in                <= 1'b0;
         dla2noc_data_len               <= 8'b0;
         dla2noc_fifo_ren               <= 8'b0;
+        dla2noc_fifo_rempty_d          <= 1'b0;
+        dla2noc_fifo_ren_d             <= 1'b0;
     end else begin
         ss                             <= ss_next;    
         cnt                            <= cnt_next;
+        cnt_d                          <= cnt;
         router_data_in                 <= router_data_in_next;
         router_valid_in                <= router_valid_in_next;
         dla2noc_data_len               <= dla2noc_data_len_next;
         dla2noc_fifo_ren               <= dla2noc_fifo_ren_next;
+        dla2noc_fifo_rempty_d          <= dla2noc_fifo_rempty;
+        dla2noc_fifo_ren_d             <= dla2noc_fifo_ren;
     end
 end
 
-logic vc0_avail = router_is_on_off_out[0] && router_is_allocatable_out[0];
-logic vc1_avail = router_is_on_off_out[1] && router_is_allocatable_out[1];
+logic vc0_avail;
+assign vc0_avail = 1'b1;
+logic vc1_avail;
+assign vc1_avail = 1'b1;
 
 always_comb
 begin
@@ -109,16 +118,19 @@ begin
     begin
         if(!dla2noc_fifo_rempty && (vc0_avail || vc1_avail))
         begin
-            ss_next                                      = FETCH_INST;
+            ss_next                                      = FETCH_HEAD;
             dla2noc_fifo_ren_next                        = 1'b1;
         end
     end
 
-    FETCH_INST:
+    FETCH_HEAD:
     begin
-        if(!dla2noc_fifo_rempty && (vc0_avail || vc1_avail))
-        begin
             ss_next                                      = HEAD_STATE;
+    end
+
+    HEAD_STATE:
+    begin
+            ss_next                                      = BODY_STATE;
             router_valid_in_next                         = 1'b1;
             router_data_in_next.flit_label               = HEAD;
             router_data_in_next.vc_id                    = vc0_avail ? 1'b0 : 1'b1;
@@ -128,37 +140,12 @@ begin
             dla2noc_data_len_next                        = fifo2router_data[FLIT_DATA_SIZE-12:FLIT_DATA_SIZE-19];
             router_data_in_next.data.head_data.head_pl   = fifo2router_data[FLIT_DATA_SIZE-20:0];
             cnt_next                                     = 8'b0;
-            dla2noc_fifo_ren_next                        = 1'b1;
-        end
     end
 
-    HEAD_STATE: 
-    if (!dla2noc_fifo_rempty && (vc0_avail || vc1_avail)) begin
-        if(dla2noc_data_len == 8'b0)
-        begin
-            ss_next                                       = TAIL_STATE;
-            router_data_in_next.flit_label                = TAIL;
-            router_data_in_next.data                      = fifo2router_data;
-            router_data_in_next.vc_id                     = vc0_avail ? 1'b0 : 1'b1;
-            cnt_next                                      = 8'b0;
-            router_valid_in_next                          = 1'b1;
-            dla2noc_fifo_ren_next                         = 1'b0;
-        end else begin
-            ss_next                                       = BODY_STATE;
-            router_data_in_next.flit_label                = BODY;
-            router_data_in_next.data                      = fifo2router_data;
-            router_data_in_next.vc_id                     = vc0_avail ? 1'b0 : 1'b1;
-            cnt_next                                      = 8'b1;
-            router_valid_in_next                          = 1'b1;
-            dla2noc_fifo_ren_next                         = 1'b1;
-        end
-    end else begin
-        ss_next = HEAD_STATE;
-    end
 
     BODY_STATE: 
-    if (!dla2noc_fifo_rempty && (vc0_avail || vc1_avail)) begin
-        if(dla2noc_data_len == cnt)
+    begin
+        if(dla2noc_data_len-1 == cnt)
         begin
             ss_next                                       = TAIL_STATE;
             router_data_in_next.flit_label                = TAIL;
@@ -172,27 +159,21 @@ begin
             router_data_in_next.flit_label                = BODY;
             router_data_in_next.data                      = fifo2router_data;
             router_data_in_next.vc_id                     = vc0_avail ? 1'b0 : 1'b1;
-            cnt_next                                      = cnt + 8'b1;
-            router_valid_in_next                          = 1'b1;
-            dla2noc_fifo_ren_next                         = 1'b1;
+            router_valid_in_next                          = dla2noc_fifo_ren_d && !dla2noc_fifo_rempty_d;
+            if (dla2noc_fifo_ren_d && !dla2noc_fifo_rempty_d) begin
+                cnt_next                                      = cnt + 8'b1;
+            end
+            if (!dla2noc_fifo_rempty && (vc0_avail || vc1_avail) && (dla2noc_data_len-2 != cnt)) begin
+                dla2noc_fifo_ren_next                         = 1'b1;
+            end
         end   
-    end else begin
-        ss_next = BODY_STATE;
     end
 
     TAIL_STATE:
-    if(!dla2noc_fifo_rempty && (vc0_avail || vc1_avail)) begin
-            ss_next                                       = FETCH_INST;
-            dla2noc_fifo_ren_next                         = 1'b1;
-        end else begin
+    begin
             ss_next                                       = IDLE;
     end  
     endcase
 end                                       
 
-// =============================================================================
 endmodule
-
-    
-
-
