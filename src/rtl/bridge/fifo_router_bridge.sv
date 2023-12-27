6e3c3676e9ef26ea27710e67ac7ee35301ef020a
MH Nie <mhnie2000@outlook.com>
2023-12-27 17:57:40 +0800

    fix dla2dla

--- src/rtl/bridge/fifo_router_bridge.sv
+++ src/rtl/bridge/fifo_router_bridge.sv
@@ -17,8 +17,8 @@ module fifo_router_bridge (
     input  logic     router_is_on_off_out,  
     input  logic     router_is_allocatable_out,
     // fifo side
-    input                                router_wrbuf_afull,
-    input                                router_wrbuf_full,
+    input  logic                         router_wrbuf_afull,
+    input  logic                         router_wrbuf_full,
     output logic                         router_wrbuf_wen,
     output logic [FLIT_DATA_SIZE-1:0]    router_wrbuf_wdata,
     input  logic                         router_rdbuf_rempty,
@@ -132,7 +132,18 @@ logic ok_to_send;
 assign ok_to_send = router_is_on_off_out;
 
 // assign router_rdbuf_ren = ok_to_send && !router_rdbuf_rempty && vc_id_assigned;
-assign router_rdbuf_ren = ok_to_send && !router_rdbuf_rempty;
+
+flit_label_t router_rdbuf_rdata_label;
+flit_label_t router_rdbuf_rdata_label_d;
+always @ (posedge clk_router or posedge rst_router) begin
+    if (rst_router) begin
+        router_rdbuf_rdata_label_d <= HEADTAIL;
+    end else begin
+        router_rdbuf_rdata_label_d <= router_rdbuf_rdata_label;
+    end
+end 
+assign router_rdbuf_ren = ok_to_send && !router_rdbuf_rempty && 
+    (router_rdbuf_rdata_label != TAIL) && (router_rdbuf_rdata_label_d != TAIL);
 
 logic router_rdbuf_vld;
 always @(posedge clk_router or posedge rst_router) begin
@@ -159,45 +170,63 @@ end
 //     end
 // end
 
-flit_label_t router_rdbuf_rdata_label;
 logic [DEST_ADDR_SIZE_X-1 : 0] router_rdbuf_rdata_dest_x; // 4
 logic [DEST_ADDR_SIZE_Y-1 : 0] router_rdbuf_rdata_dest_y; // 4
 logic [DEST_ADDR_SIZE_L-1 : 0] router_rdbuf_rdata_dest_l; // 3
 logic [DEST_ADDR_SIZE_X+DEST_ADDR_SIZE_Y+1-1:0] noc2dla_grnt_HEAD_pl;
-assign router_rdbuf_rdata_label = flit_label_t'(router_rdbuf_rdata[FLIT_TOTAL_SIZE-1:FLIT_DATA_SIZE]);
+assign router_rdbuf_rdata_label = router_rdbuf_vld?flit_label_t'(router_rdbuf_rdata[FLIT_TOTAL_SIZE-1:FLIT_DATA_SIZE]):HEADTAIL;
 assign router_rdbuf_rdata_dest_l = router_rdbuf_rdata[0                                +:DEST_ADDR_SIZE_L];
 assign router_rdbuf_rdata_dest_y = router_rdbuf_rdata[DEST_ADDR_SIZE_L                 +:DEST_ADDR_SIZE_Y];
 assign router_rdbuf_rdata_dest_x = router_rdbuf_rdata[DEST_ADDR_SIZE_L+DEST_ADDR_SIZE_Y+:DEST_ADDR_SIZE_X];
 assign noc2dla_grnt_HEAD_pl = router_rdbuf_rdata[DEST_ADDR_SIZE_L+DEST_ADDR_SIZE_Y+DEST_ADDR_SIZE_X+:(DEST_ADDR_SIZE_Y+DEST_ADDR_SIZE_X+2)];
 
-
-always @(posedge clk_router or posedge rst_router) begin
-    if (rst_router) begin
-        router_data_in.flit_label                     <= HEADTAIL;
-        router_data_in.data                           <= '0;
-        // router_data_in.vc_id                          <= 1'b0;
-        router_valid_in                               <= 1'b0;
-    end else begin
-        if (router_rdbuf_vld) begin
-            // router_data_in.vc_id                      <= vc_id;
-            router_valid_in                           <= 1'b1;
-            router_data_in.flit_label                 <= router_rdbuf_rdata_label;
-            if (router_rdbuf_rdata_label == HEAD || router_rdbuf_rdata_label == HEADTAIL) begin
-                router_data_in.data.head_data.x_dest  <= router_rdbuf_rdata_dest_x;
-                router_data_in.data.head_data.y_dest  <= router_rdbuf_rdata_dest_y;
-                router_data_in.data.head_data.l_dest  <= router_rdbuf_rdata_dest_l;
-                if (router_rdbuf_rdata_label == HEAD) begin    
-                    router_data_in.data.head_data.head_pl <= '0;
-                end else begin
-                    router_data_in.data.head_data.head_pl <= noc2dla_grnt_HEAD_pl;
-                end
-            end else begin
-                router_data_in.data                   <= router_rdbuf_rdata;
-            end
+assign router_valid_in = router_rdbuf_vld;
+assign router_data_in.flit_label = router_rdbuf_rdata_label;
+always@(*) begin
+    if (router_rdbuf_rdata_label == HEAD || router_rdbuf_rdata_label == HEADTAIL) begin
+        router_data_in.data.head_data.x_dest  = router_rdbuf_rdata_dest_x;
+        router_data_in.data.head_data.y_dest  = router_rdbuf_rdata_dest_y;
+        router_data_in.data.head_data.l_dest  = router_rdbuf_rdata_dest_l;
+        if (router_rdbuf_rdata_label == HEAD) begin    
+            router_data_in.data.head_data.head_pl = '0;
         end else begin
-            router_valid_in                           <= 1'b0;
+            router_data_in.data.head_data.head_pl = noc2dla_grnt_HEAD_pl;
         end
+    end else begin
+        router_data_in.data                   = router_rdbuf_rdata;
     end
 end
 
+
+// always @(posedge clk_router or posedge rst_router) begin
+//     if (rst_router) begin
+//         router_data_in.flit_label                     <= HEADTAIL;
+//         router_data_in.data                           <= '0;
+//         // router_data_in.vc_id                          <= 1'b0;
+//         router_valid_in                               <= 1'b0;
+//     end else begin
+//         if (router_rdbuf_ren) begin
+//             // router_data_in.vc_id                      <= vc_id;
+//             router_valid_in                           <= 1'b1;
+//             router_data_in.flit_label                 <= router_rdbuf_rdata_label;
+//             if (router_rdbuf_rdata_label == HEAD || router_rdbuf_rdata_label == HEADTAIL) begin
+//                 router_data_in.data.head_data.x_dest  <= router_rdbuf_rdata_dest_x;
+//                 router_data_in.data.head_data.y_dest  <= router_rdbuf_rdata_dest_y;
+//                 router_data_in.data.head_data.l_dest  <= router_rdbuf_rdata_dest_l;
+//                 if (router_rdbuf_rdata_label == HEAD) begin    
+//                     router_data_in.data.head_data.head_pl <= '0;
+//                 end else begin
+//                     router_data_in.data.head_data.head_pl <= noc2dla_grnt_HEAD_pl;
+//                 end
+//             end else begin
+//                 router_data_in.data                   <= router_rdbuf_rdata;
+//             end
+//         end else begin
+//             router_valid_in                           <= 1'b0;
+//         end
+//     end
+// end
+
 endmodule
+
+`resetall
